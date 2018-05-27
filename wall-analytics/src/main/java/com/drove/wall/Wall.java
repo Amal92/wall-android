5eeb8e790a357d3ecfbcc04a3593bd27f0e53d5b
amal <amalpaul0492@gmail.com>
2018-05-27 11:26:29 +0530

    added crash reporting

--- wall-analytics/src/main/java/com/drove/wall/Wall.java
+++ wall-analytics/src/main/java/com/drove/wall/Wall.java
@@ -1,9 +1,15 @@
 package com.drove.wall;
 
-import android.app.Activity;
+import android.app.Application;
 import android.content.Context;
 import android.os.Build;
 import android.util.Log;
+
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.config.CoreConfigurationBuilder;
+import org.acra.config.MailSenderConfigurationBuilder;
+import org.acra.data.StringFormat;
 
 import java.lang.reflect.Field;
 
@@ -12,16 +18,19 @@ import java.lang.reflect.Field;
  */
 
 public class Wall {
-    public static void with(Activity activity){
-        Log.d("Amal", "App version: " + getBuildConfigValue(activity,"VERSION_NAME"));
-        Log.d("Amal", "Android version: " + Build.VERSION.RELEASE);
-        Log.d("Amal", "Brand: " + Build.BRAND);
-        Log.d("Amal", "Hardware: " + Build.HARDWARE);
-        Log.d("Amal", "Manufacture: " + Build.MANUFACTURER);
-        Log.d("Amal", "Model: " + Build.MODEL);
+
+    final static String TAG = "wall/class";
+
+    public static void with(Application application) {
+        Log.d(TAG, "App version: " + getBuildConfigValue(application.getApplicationContext(), "VERSION_NAME"));
+        Log.d(TAG, "Android version: " + Build.VERSION.RELEASE);
+        Log.d(TAG, "Brand: " + Build.BRAND);
+        Log.d(TAG, "Hardware: " + Build.HARDWARE);
+        Log.d(TAG, "Manufacture: " + Build.MANUFACTURER);
+        Log.d(TAG, "Model: " + Build.MODEL);
     }
 
-    public static Object getBuildConfigValue(Context context, String fieldName) {
+    private static Object getBuildConfigValue(Context context, String fieldName) {
         try {
             Class<?> clazz = Class.forName(context.getPackageName() + ".BuildConfig");
             Field field = clazz.getField(fieldName);
@@ -35,4 +44,43 @@ public class Wall {
         }
         return null;
     }
+
+    private static Class getBuildClass(Context context) {
+        try {
+            Class<?> clazz = Class.forName(context.getPackageName() + ".BuildConfig");
+
+            return clazz;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void setUpAndInstallCrashLogging(Application application) {
+
+        CoreConfigurationBuilder builder = new CoreConfigurationBuilder(application);
+        builder.setBuildConfigClass(getBuildClass(application)).setReportFormat(StringFormat.JSON);
+        builder.setReportContent(ReportField.APP_VERSION_CODE,
+                ReportField.APP_VERSION_NAME,
+                ReportField.ANDROID_VERSION,
+                ReportField.PACKAGE_NAME,
+                ReportField.REPORT_ID,
+                ReportField.BUILD,
+                ReportField.STACK_TRACE,
+                ReportField.BRAND,
+                ReportField.PHONE_MODEL,
+                ReportField.USER_CRASH_DATE,
+                ReportField.AVAILABLE_MEM_SIZE,
+                ReportField.TOTAL_MEM_SIZE);
+        builder.setReportField(ReportField.CUSTOM_DATA,true);
+
+
+
+        builder.getPluginConfigurationBuilder(MailSenderConfigurationBuilder.class).setEnabled(true)
+                .setMailTo("amalpaul0492@gmail.com").setSubject("CrashReport");
+
+
+        ACRA.init(application,builder);
+    }
+
 }
